88e59711308c696c9440d65b96a366bcaf26791f
Asif <muhammadasifazmi@gmail.com>
2023-03-18 20:27:18 +0530

    Assignment submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+    public static void main(String args[]){
+        Product p=new Product();
+        int a=p.product(5,7);
+        int b=p.product(5,7,8);
+        double c=p.product(5.5,6.5);
+    }
 }
